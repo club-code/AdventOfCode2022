77c923300a3dc4de917f45269488bcbf09e88e86
Promethee Toneatti <promethee.toneatti@telecom-sudparis.eu>
2022-11-30 22:40:10 +0100

    clean cpp folder

--- CPP/AdventOfAkasuna/AdventOfAkasuna/AdventOfAkasuna.cpp
+++ CPP/AdventOfAkasuna/AdventOfAkasuna/AdventOfAkasuna.cpp
@@ -1,4 +1,4 @@
-// AdventOfCode.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+// AdventOfAkasuna.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
 //
 
 #include <iostream>
