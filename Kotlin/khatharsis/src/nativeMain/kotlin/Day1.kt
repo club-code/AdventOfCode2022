921ada4e1c6aed2fae7ce85a04fe6dd7145a21f4
khatharsis <hguelque@gmail.com>
2022-12-01 12:50:15 +0100

    Made utility function

--- Kotlin/khatharsis/src/nativeMain/kotlin/Day1.kt
+++ Kotlin/khatharsis/src/nativeMain/kotlin/Day1.kt
@@ -1,13 +1,5 @@
 class Day1 : DaySolver(1, "Calorie Counting") {
-    private val newData = data.fold(mutableListOf(mutableListOf<String>())) { acc, it ->
-        if (it.isBlank()) {
-            acc.add(mutableListOf<String>())
-            return@fold acc
-        } else {
-            acc.last().add(it)
-            return@fold acc
-        }
-    }.map { it.sumOf { nb->nb.toLong() } }
+    private val newData = data.groupBySeparatorBlank().map { it.sumOf { nb->nb.toLong() } }
 
     override fun firstPart(): String {
         return newData.max().toString()
@@ -15,5 +7,15 @@ class Day1 : DaySolver(1, "Calorie Counting") {
 
     override fun secondPart(): String {
         return newData.sortedDescending().take(3).sum().toString()
+    }
+}
+
+fun List<String>.groupBySeparatorBlank() = this.fold(mutableListOf(mutableListOf<String>())) { acc, it ->
+    if (it.isBlank()) {
+        acc.add(mutableListOf<String>())
+        return@fold acc
+    } else {
+        acc.last().add(it)
+        return@fold acc
     }
 }
