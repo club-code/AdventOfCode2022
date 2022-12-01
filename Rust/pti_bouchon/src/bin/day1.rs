ef0ea6eaf25be458c902b57a5d7a6c52520513dc
PtiBouchon <lopite2@live.fr>
2022-12-01 11:40:25 +0100

    Day1 commit

--- Rust/pti_bouchon/src/bin/day1.rs
+++ Rust/pti_bouchon/src/bin/day1.rs
@@ -1,25 +1,33 @@
 use pti_bouchon::{print_solutions, Solver};
-use anyhow::Result;
 
 fn main() {
-    print_solutions(Day1, "resources/test.txt")
+    print_solutions(Day1, "resources/day1.txt")
 }
 
 struct Day1;
 
 impl Solver for Day1 {
-    type ParsedInput = Vec<usize>;
+    type ParsedInput = Vec<Vec<usize>>;
     type Output = usize;
 
-    fn parse(input: impl Iterator<Item = String>) -> Self::ParsedInput {
-        input.map(|s| s.parse().unwrap()).collect()
+    fn parse(input: Vec<String>) -> Self::ParsedInput {
+        input.split(|line| line.is_empty()).map(|elf_bag| elf_bag.iter().map(|calorie| calorie.parse().unwrap()).collect()).collect()
     }
 
-    fn part1(&self, parsed_input: &Self::ParsedInput) -> Result<Self::Output> {
-        Ok(parsed_input.capacity())
+    fn part1(parsed_input: &Self::ParsedInput) -> Self::Output {
+        parsed_input.iter().map(|calories| calories.iter().sum()).max().unwrap()
     }
 
-     fn part2(&self, parsed_input: &Self::ParsedInput) -> Result<Self::Output> {
-         self.part1(parsed_input)
+     fn part2(parsed_input: &Self::ParsedInput) -> Self::Output {
+         let elf_calories: Vec<usize> = parsed_input.iter().map(|calories| calories.iter().sum()).collect();
+         let mut max_calories: [usize; 3] = [elf_calories[0], elf_calories[1], elf_calories[2]];
+         for calories in elf_calories {
+            if calories > max_calories[0] {
+                max_calories[2] = max_calories[1];
+                max_calories[1] = max_calories[0];
+                max_calories[0] = calories;
+            }
+         }
+         max_calories.into_iter().sum()
      }
 }
--- Rust/pti_bouchon/src/lib.rs
+++ Rust/pti_bouchon/src/lib.rs
@@ -2,35 +2,18 @@ use std::fmt;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
-use anyhow::{Error, Result};
-
-#[derive(Debug)]
-struct UnImplemented;
-
-impl fmt::Display for UnImplemented {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Not Implemented !")
-    }
-}
-
-impl std::error::Error for UnImplemented {}
-
 pub trait Solver {
     type ParsedInput;
     type Output: fmt::Debug;
 
-    fn parse(input: impl Iterator<Item = String>) -> Self::ParsedInput;
-    fn part1(&self, parsed_input: &Self::ParsedInput) -> Result<Self::Output>;
-    fn part2(&self, _parsed_input: &Self::ParsedInput) -> Result<Self::Output> { Err(Error::from(UnImplemented)) }
+    fn parse(input: Vec<String>) -> Self::ParsedInput;
+    fn part1(_parsed_input: &Self::ParsedInput) -> Self::Output;
+    fn part2(_parsed_input: &Self::ParsedInput) -> Self::Output;
 }
 
-pub fn print_solutions<S: Solver>(solver: S, path: impl AsRef<Path>) {
-    let input = BufReader::new(File::open(path).unwrap()).lines().map(|l| l.unwrap());
+pub fn print_solutions<S: Solver>(_: S, path: impl AsRef<Path>) {
+    let input = BufReader::new(File::open(path).unwrap()).lines().map(|l| l.unwrap()).collect();
     let parsed_input = S::parse(input);
-    println!("Part1: {:?}", solver.part1(&parsed_input).unwrap());
-    let solution_part2 = solver.part2(&parsed_input);
-    match solution_part2 {
-        Err(err) if err.is::<UnImplemented>()  => (),
-        _ => println!("Part2: {:?}", solution_part2.unwrap()),
-    }
+    println!("Part1: {:?}", S::part1(&parsed_input));
+    println!("Part2: {:?}", S::part2(&parsed_input));
 }
