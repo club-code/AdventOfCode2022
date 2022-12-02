e29d07de2dc1c8abc5a3bd85d4960f25c8372f20
Guit0u <guillaume.branche@gmail.com>
2022-12-02 01:08:34 +0100

    day one

---
+++ CSharp/Guitou/AdventOfCode2022/Solutions/Day1.cs
@@ -0,0 +1,27 @@
+﻿string inputsPath = "C:\\Users\\guill\\Desktop\\AdventOfCode2022\\CSharp\\Guitou\\AdventOfCode2022\\Inputs\\"; //change to your own
+
+string input = File.ReadAllText(inputsPath + "Input1.txt");
+
+#region Part one
+string[] elves = input.Split(Environment.NewLine + Environment.NewLine);
+int result = elves.Select(elf =>
+{
+    string[] calories = elf.Split(Environment.NewLine);
+    return calories.Select(x => int.Parse(x)).Sum();
+}).Max();
+
+Console.WriteLine(result);
+#endregion
+
+#region Part Two
+result = elves.Select(elf =>
+    { 
+        string[] calories = elf.Split(Environment.NewLine); 
+        return calories.Select(x => int.Parse(x)).Sum(); 
+    })
+    .OrderByDescending(x => x)
+    .Take(3)
+    .Sum();
+
+Console.WriteLine(result);
+#endregion
