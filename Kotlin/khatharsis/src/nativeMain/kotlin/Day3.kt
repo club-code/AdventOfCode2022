685a174bebda8b2fbd0b7aa21b2e5d67c6374fa4
khatharsis <hguelque@gmail.com>
2022-12-03 17:13:02 +0100

    Optimization

--- Kotlin/khatharsis/src/nativeMain/kotlin/Day3.kt
+++ Kotlin/khatharsis/src/nativeMain/kotlin/Day3.kt
@@ -1,21 +1,18 @@
 class Day3 : DaySolver(3, "Rucksack Reorganization") {
     override fun firstPart() =
-        data.map {
+        data.sumOf {s->
             // On coupe la string en 2
-            it.chunked(it.length / 2)
-        }.map {
-            // On ne garde que les char qui sont présents dans les deux Strings
-            it[0].filter { c -> it[1].contains(c) }.toSet()
-        }.sumOf { it.sumOf { c -> c.priority() } }.toString()
-    // Et on somme
+            s.chunked(s.length / 2).let {
+                // On ne prends que le char qui est contenu dans les deux Strings
+                it[0].first { c -> it[1].contains(c) }
+            }.priority()
+        }.toString()
 
     override fun secondPart() = data.chunked(3) // On fait les groupes de trois
-        .map { group ->
+        .sumOf { group ->
             // Puis on trouve le char présent dans tous les groups
-        group[0].filter { c -> group[1].contains(c) && group[2].contains(c) }.first()
-    }.sumOf {
-        it.priority()
-    }.toString()
+            group[0].first { c -> group[1].contains(c) && group[2].contains(c) }.priority()
+        }.toString()
 
     // On pourrait utiliser des sets et des .intersect mais c'et beaucoup plus long (environ 20 fois plus)
 
