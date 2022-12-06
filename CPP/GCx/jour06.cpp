e4f4773b43f6e79a7aace8ee9fa9ddd597847918
GuillaumeCx <guillaume.caucheteux@hotmail.fr>
2022-12-06 23:50:46 +0100

    Je sais pas pourquoi cet essai a réussi là où le précédent non, mais bon

--- CPP/GCx/jour06.cpp
+++ CPP/GCx/jour06.cpp
@@ -3,16 +3,47 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-int jour06partie1(std::string line){
+
+bool check(std::string line, std::unordered_map<char, int> content, int current, int message_length){
+    for (int i = current - message_length + 1;  i <= current; i++){
+        if (content[line[i]]!=1){
+            return false;
+        }
+    }
+    return true;
+}
+
+std::unordered_map<char, int> init(std::string line, int message_length){
     std::unordered_map<char, int> content;
-    for (int i = 0; i < 4; i++){
+    for (int i = 0; i < message_length; i++){
         content[line[i]]++;
     }
+    return content;
+}
+
+int jour06partie1(std::string line){
+    auto content = init(line, 4);
     if (content[line[0]]+content[line[1]]+content[line[2]]+content[line[3]]!=4)
     for (size_t j = 4; j < line.length(); j++ ){
         content[line[j-4]]--;
         content[line[j]]++;
+        if (check(line, content, j, 4)){
+            std::cout << "True : "<< j +1 <<std::endl;
+        }
         if (content[line[j]]+content[line[j-1]]+content[line[j-2]]+content[line[j-3]]==4){
+            std::cout << line[j-3]<<line[j-2]<<line[j-1]<<line[j]<<std::endl;
+            return j+1;
+        }
+    }
+    return 1;
+}
+
+int jour06(std::string line, int message_length){
+    auto content = init(line, message_length);
+    for (size_t j = message_length; j < line.length(); j++){
+        content[line[j-message_length]]--;
+        content[line[j]]++;
+        if (check(line, content, j, message_length)){
             return j+1;
         }
     }
@@ -31,12 +62,16 @@ int main (int argc, char** argv){
         }
         
         auto lines = readFile(path);
-        if (std::strcmp(argv[1], "1")==0){
-            for (auto l : lines){
-                int res = jour06partie1(l);
-                std::cout << l << std::endl;
-                std::cout << "Res = "<< res<<std::endl <<std::endl;
-            } 
+        int message_length = 4;
+        if (std::strcmp(argv[1], "1")==0){ 
+            message_length = 4;
+        } else if (std::strcmp(argv[1], "2")==0){
+            message_length = 14;
         }
+        for (auto l : lines){
+            int res = jour06(l, message_length);
+            std::cout << l << std::endl;
+            std::cout << "Res = "<< res<<std::endl <<std::endl;
+        } 
     }
 }
