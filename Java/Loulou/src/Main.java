1b09bd3e37270fd64ac91aa097e15b91cb171f98
Louis Delyon <louis.delyon.si@gmail.com>
2022-12-08 13:19:17 +0100

    Loulou Day8

--- Java/Loulou/src/Main.java
+++ Java/Loulou/src/Main.java
@@ -1,12 +1,7 @@
-import Day7.Day7;
-
-import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
-import java.util.Vector;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        new Day7().hard();
+        new Day8().hard();
     }
 }
