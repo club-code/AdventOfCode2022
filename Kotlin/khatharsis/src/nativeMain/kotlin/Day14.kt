9fdc5f4c9a5d9ab2fa592d217eeb81c9cc72505b
khatharsis <hguelque@gmail.com>
2022-12-14 15:14:05 +0100

    Optimisation and comments.

--- Kotlin/khatharsis/src/nativeMain/kotlin/Day14.kt
+++ Kotlin/khatharsis/src/nativeMain/kotlin/Day14.kt
@@ -25,10 +25,18 @@ class Day14 : DaySolver(14, "Regolith Reservoir") {
 
     private val pouringPoint = 500 to 0
     private val abyssAbscissa = world.filterValues { it is Block.Rock }.keys.maxOf { it.second }
-    // Any block of sand falling below that shall be lost to the abyss
 
+    // Any block of sand falling below that shall be lost to the abyss
+    private val path = mutableListOf<Vector>()
+    // This is a way to remember the current direct path of our sand blocks
+
+    /**
+     * Given a vector (current position), returns the next position this block of sand should go.
+     */
     private fun sandGoesDownward(curPosition: Vector): Vector {
+        // Rocky floor
         if (curPosition.second == abyssAbscissa + 1) return curPosition
+        // Go down, left or right
         for (delta in listOf(0, -1, 1)) {
             // Check for rock floor
             val tryNewPosition = curPosition.first + delta to curPosition.second + 1
@@ -36,28 +44,46 @@ class Day14 : DaySolver(14, "Regolith Reservoir") {
                 return tryNewPosition
             }
         }
+        // If nothing worked, shouldn't move
         return curPosition
     }
 
     private fun placeSand(maxFloor: Int): Vector {
-        var curPosition = pouringPoint
+        // Let's start where we left off (or at the pouring Point)
+        var curPosition = path.lastOrNull() ?: pouringPoint
+
+        // While our current position isn't a stable one
+        // And we're not pass the maxFloor given
         while (curPosition != sandGoesDownward(curPosition).also {
                 curPosition = it
-            } && curPosition.second < maxFloor) {
-            /*Do Nothing*/
+            } // This is to update the curPosition
+            && curPosition.second < maxFloor) {
+            // Let's add this position to the path.
+            path.add(curPosition)
         }
-        if (curPosition.second < maxFloor) world[curPosition] = Block.Sand
+        if (curPosition.second < maxFloor) {
+            // The last position is the current one
+            // But it's going to be used now, so remove it
+            path.removeLastOrNull()
+            world[curPosition] = Block.Sand
+        }
+        // Return the position where the block was placed
+        // (Or, if curPosition.second >= maxFloor, if it wasn't placed !)
         return curPosition
     }
 
 
     override fun firstPart(): String {
-        while (placeSand(abyssAbscissa).second < abyssAbscissa) {/*Do Nothing*/ }
+        while (placeSand(abyssAbscissa).second < abyssAbscissa) {
+            //Do Nothing
+        }
         return world.count { it.value is Block.Sand }.toString()
     }
 
     override fun secondPart(): String {
-        while (placeSand(abyssAbscissa+2) != pouringPoint) {/*Do Nothing*/}
+        while (placeSand(abyssAbscissa + 2) != pouringPoint) {
+            //Do Nothing
+        }
         return world.count { it.value is Block.Sand }.toString()
     }
 }
