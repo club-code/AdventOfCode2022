0cefcaedbab4149c66f92888d3213b46ba044c86
Guillaume Caucheteux <guillaume.caucheteux@hotmail.fr>
2023-03-11 21:45:14 +0100

    Jour 18 partie 1 : muy bien !

--- CPP/GCx/jour18.cpp
+++ CPP/GCx/jour18.cpp
@@ -28,6 +28,12 @@ struct Triplet{
         y = std::stoi(aux[1]);
         z = std::stoi(aux[2]);
     }
+
+    Triplet(int arg_x, int arg_y, int arg_z){
+        x = arg_x;
+        y = arg_y;
+        z = arg_z;
+    }
 };
 
 inline std::ostream& operator<<(std::ostream& os, const Triplet s){
@@ -35,18 +41,36 @@ inline std::ostream& operator<<(std::ostream& os, const Triplet s){
     return os;
 }
 
+inline bool operator<(const Triplet t1, const Triplet t2){
+    return (t1.x < t2.x || (t1.x == t2.x && t1.y < t2.y) || (t1.x == t2.x && t1.y == t2.y && t1.z < t2.z)); 
+}
+
 bool are_adjacent(const Triplet t, const Triplet u){
     return std::abs(t.x-u.x) + std::abs(t.y-u.y) + std::abs(t.z-u.z) <=1;
 }
 
+int get_number_neighbours(std::set<Triplet> cubes, Triplet t){
+    int voisins = 0;
+    for (auto aux : {Triplet{1,0,0}, Triplet{-1, 0, 0}, Triplet{0, 1, 0}, Triplet{0, -1, 0}, Triplet{0, 0, 1}, Triplet{0, 0, -1}}){
+        if (cubes.find(Triplet{t.x+aux.x, t.y+aux.y, t.z+aux.z})!=cubes.end()){
+            voisins++;
+            std::cout << Triplet{t.x+aux.x, t.y+aux.y, t.z+aux.z} << " est voisin de "<<t <<std::endl;
+        }
+    }
+    std::cout << t << " : " << voisins << " voisins"<<std::endl;
+    return voisins;
+} 
+
 int jour18partie1(std::vector<std::string> lines){
     std::set<Triplet> cubes;
+    int intersections;
     for (auto line : lines){
-        std::cout << Triplet{line} << std::endl;
-        cubes.insert(Triplet{line});
+        std::cout << Triplet{line} << std::endl; 
+        cubes.insert(Triplet(line));
+        intersections += get_number_neighbours(cubes, Triplet{line});
     }
     
-    return 0;
+    return 6*cubes.size() - 2*intersections;
 }
 
 int main(int argc, char** argv){
@@ -64,7 +88,8 @@ int main(int argc, char** argv){
         for (std::string line : lines){
             std::cout << line << std::endl;
         }
-        jour18partie1(lines);
+        int res = jour18partie1(lines);
+        std::cout << "Res = "<<res <<std::endl;
     }
     return 0;
 }
